115217c8586b0216caeeb44f0a12618d7a29d67d
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-01-07 13:57:41 -0300

    removed dropwond menu from home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,34 @@
-const Home = () => (
-  <p>dsadsadsa</p>
-);
+/* eslint-disable no-unused-vars */
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import DropdownMenu from '../components/dropdownMenu';
+import { fetchTopTenItems } from '../redux/slices/itemsSlice';
+
+const Home = () => {
+  const dispatch = useDispatch();
+  const { loading, itemsArr } = useSelector((store) => store.items);
+
+  useEffect(() => {
+    dispatch(fetchTopTenItems());
+  }, []);
+
+  return (
+    <>
+      <DropdownMenu />
+      {!loading && (
+      <div>
+        <ul>
+          {itemsArr.map((item) => (
+            <li key={item.name}>
+              <p>{item.name}</p>
+              <img src={item.image} alt={item.name} className="h-10" />
+            </li>
+          ))}
+        </ul>
+      </div>
+      )}
+    </>
+  );
+};
 
 export default Home;
