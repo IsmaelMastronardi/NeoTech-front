e64091b9c581dcbdcc3562f72b4a51e049064b02
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-01-07 13:57:10 -0300

    add categories slice

--- src/redux/slices/categoriesSlice.js
+++ src/redux/slices/categoriesSlice.js
@@ -1,9 +1,18 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-unused-vars */
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-// const fetchCategories = createAsyncThunk(
+const url = 'http://localhost:3000/api/v1/categories';
 
-// );
+export const fetchCategories = createAsyncThunk('categories/index', async () => {
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    throw new Error('Error');
+  }
+});
 
 const initialState = {
   loading: true,
@@ -14,13 +23,15 @@ const categoriesSlice = createSlice({
   name: 'categories',
   initialState,
   reducers: {},
-  // extraReducers: (builder) => {
-  //   // builder.addCase(fetchCategories.fulfilled, (state, action) => {
-  //   //   // state.categoriesArr.push(action);
-  //   //   console.log(state);
-  //   //   console.log(action);
-  //   // });
-  // },
+  extraReducers: (builder) => {
+    builder.addCase(fetchCategories.fulfilled, (state, action) => {
+      state.categoriesArr = [];
+      action.payload.forEach((el) => {
+        state.categoriesArr.push(el);
+      });
+      state.loading = false;
+    });
+  },
 });
 
 export default categoriesSlice.reducer;
