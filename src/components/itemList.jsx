c1bc1391bba178d4759a0678db17e51c762cd2a8
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-01-08 14:49:11 -0300

    add item list loading. add item list

--- src/components/itemList.jsx
+++ src/components/itemList.jsx
@@ -1,7 +1,36 @@
+/* eslint-disable no-unused-vars */
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchTopTenItems } from '../redux/slices/itemsSlice';
+
 const ItemList = () => {
-  console.log('a');
+  const { loading, itemsArr } = useSelector((store) => store.items);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchTopTenItems());
+  }, []);
+
+  if (loading) {
+    return (
+      <p>loading ...</p>
+    );
+  }
+
   return (
-    <p>item list</p>
+    <>
+      <div>
+        <ul>
+          {itemsArr.map((item) => (
+            <li key={item.name}>
+              <p>{item.name}</p>
+              <img src={item.image} alt={item.name} className="h-10" />
+            </li>
+          ))}
+        </ul>
+      </div>
+
+    </>
   );
 };
 
