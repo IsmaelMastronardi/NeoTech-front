d5ecccd7075b7a7d8179897f68ef22eb8e39c55f
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-01-08 14:51:01 -0300

    change url. add fetchCategoryItems

--- src/redux/slices/itemsSlice.js
+++ src/redux/slices/itemsSlice.js
@@ -2,11 +2,20 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const url = 'http://localhost:3000/api/v1/items/recentlyAdded';
+const url = 'http://localhost:3000/api/v1/';
 
 export const fetchTopTenItems = createAsyncThunk('items/topTen', async () => {
   try {
-    const response = await axios(url);
+    const response = await axios(`${url}items/recentlyAdded`);
+    return response.data;
+  } catch (error) {
+    throw new Error('error');
+  }
+});
+
+export const fetchCategoryitems = createAsyncThunk('categories/show', async (categoryId) => {
+  try {
+    const response = await axios(`${url}categories/${categoryId}`);
     return response.data;
   } catch (error) {
     throw new Error('error');
@@ -23,11 +32,15 @@ const itemSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(fetchTopTenItems.fulfilled, (state, action) => {
-      state.itemsArr = action.payload;
-      state.loading = false;
-      console.log(state.itemsArr);
-    });
+    builder
+      .addCase(fetchTopTenItems.fulfilled, (state, action) => {
+        state.itemsArr = action.payload;
+        state.loading = false;
+      })
+      .addCase(fetchCategoryitems.fulfilled, (state, action) => {
+        state.itemsArr = action.payload;
+        state.loading = false;
+      });
   },
 
 });
