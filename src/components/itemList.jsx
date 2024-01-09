c49a9c254adfcf920cf0971a8fa577d3ca1cd044
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-01-09 19:24:30 -0300

    add orderList functionality to itemList

--- src/components/itemList.jsx
+++ src/components/itemList.jsx
@@ -1,15 +1,36 @@
+/* eslint-disable consistent-return */
 /* eslint-disable no-unused-vars */
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchTopTenItems } from '../redux/slices/itemsSlice';
 
 const ItemList = () => {
   const { loading, itemsArr } = useSelector((store) => store.items);
+  const [orderMenuOpen, setOrderMenuOpen] = useState(false);
+  const [order, setOrder] = useState('all');
   const dispatch = useDispatch();
+  const orderByArr = ['all', 'Higher Price', 'Lower Price'];
 
   useEffect(() => {
     dispatch(fetchTopTenItems());
   }, []);
+
+  const chooseOrder = (val) => {
+    setOrder(val);
+  };
+
+  const toggleOrderMenu = () => {
+    setOrderMenuOpen(!orderMenuOpen);
+  };
+
+  const orderComparator = (a, b) => {
+    if (order === 'Lower Price') {
+      return (a.price - b.price);
+    }
+    if (order === 'Higher Price') {
+      return (b.price - a.price);
+    }
+  };
 
   if (loading) {
     return (
@@ -17,21 +38,52 @@ const ItemList = () => {
     );
   }
 
-  return (
-    <>
-      <div>
-        <ul>
-          {itemsArr.map((item) => (
-            <li key={item.name}>
-              <p>{item.name}</p>
-              <img src={item.image} alt={item.name} className="h-10" />
-            </li>
-          ))}
-        </ul>
-      </div>
+  if (!loading) {
+    const itemsArrCopy = [...itemsArr];
+    if (order !== 'all') {
+      itemsArrCopy.sort(orderComparator);
+    }
+    return (
+      <>
+        <div className="m-4">
+          <button
+            type="button"
+            onClick={() => toggleOrderMenu()}
+          >
+            Order By
+          </button>
+          <div>
+            {orderMenuOpen && (
+            <ul>
+              {orderByArr.map((order) => (
+                <li key={order}>
+                  <button
+                    type="button"
+                    onClick={() => chooseOrder(order)}
+                  >
+                    {order}
+                  </button>
+                </li>
+              ))}
+            </ul>
+            )}
+          </div>
+        </div>
+        <div className="">
+          <ul className="flex flex-row flex-wrap gap-4">
+            {itemsArrCopy.map((item) => (
+              <li key={item.name} className="w-1/5 border-2">
+                <p>{item.name}</p>
+                <img src={item.image} alt={item.name} className="h-10" />
+                <p>{item.price}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
 
-    </>
-  );
+      </>
+    );
+  }
 };
 
 export default ItemList;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,18 @@
+/* eslint-disable no-unused-vars */
+import { useState } from 'react';
 import ItemList from '../components/itemList';
 import CategoriesMenu from '../components/categoriesMenu';
+import FilterMenu from '../components/filtersMenu';
 
 const Home = () => (
   <>
-    <CategoriesMenu />
-    <ItemList />
+    <div className="flex flex-row gap-10 mb-10">
+      <CategoriesMenu />
+      <FilterMenu />
+    </div>
+    <div>
+      <ItemList />
+    </div>
   </>
 );
 
