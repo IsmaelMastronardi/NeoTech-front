c944f50095142548b1c5fffc3033cf9b2ea0cb83
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-01-09 19:23:46 -0300

    add fildet component

---
+++ src/components/filtersMenu.jsx
@@ -0,0 +1,13 @@
+const FilterMenu = () => {
+  console.log('a');
+  return (
+    <button
+      type="button"
+      className="border-2"
+    >
+      filters
+    </button>
+  );
+};
+
+export default FilterMenu;
