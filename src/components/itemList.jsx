d25d63c3d91830cb5e326906bf3118c3042b83fe
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-01-10 16:53:46 -0300

    add simple styles for itemList

--- src/components/itemList.jsx
+++ src/components/itemList.jsx
@@ -45,37 +45,46 @@ const ItemList = () => {
     }
     return (
       <>
-        <div className="m-4">
-          <button
-            type="button"
-            onClick={() => toggleOrderMenu()}
-          >
-            Order By
-          </button>
-          <div>
-            {orderMenuOpen && (
-            <ul>
-              {orderByArr.map((order) => (
-                <li key={order}>
-                  <button
-                    type="button"
-                    onClick={() => chooseOrder(order)}
-                  >
-                    {order}
-                  </button>
-                </li>
-              ))}
-            </ul>
-            )}
+        <div className="relative w-full p-4">
+          <div className="flex flex-col p-2 bg-white rounded-xl">
+            <button
+              type="button"
+              className="w-full text-start"
+              onClick={() => toggleOrderMenu()}
+            >
+              Order By
+            </button>
+            <div className="absolute bg-white left-4 right-4 top-14">
+              {orderMenuOpen && (
+              <ul className="z-10 flex flex-col gap-2">
+                {orderByArr.map((order) => (
+                  <li key={order} className="p-2">
+                    <button
+                      type="button"
+                      onClick={() => {
+                        chooseOrder(order);
+                        toggleOrderMenu();
+                      }}
+                    >
+                      {order}
+                    </button>
+                  </li>
+                ))}
+              </ul>
+              )}
+            </div>
           </div>
         </div>
-        <div className="">
-          <ul className="flex flex-row flex-wrap gap-4">
+        <div className="w-full p-4 ">
+          <ul className="flex flex-col content-center justify-center w-full bg-white rounded-xl">
             {itemsArrCopy.map((item) => (
-              <li key={item.name} className="w-1/5 border-2">
-                <p>{item.name}</p>
-                <img src={item.image} alt={item.name} className="h-10" />
-                <p>{item.price}</p>
+              <li key={item.name} className="flex flex-row content-center justify-between p-2 text-center border-1 ">
+                <div className="w-1/3 h-full"><img src={item.image} alt={item.name} className="" /></div>
+                <div className="w-2/3">
+                  <p>{item.name}</p>
+                  <p>{item.price}</p>
+                  <button type="button" className="">Add To cart</button>
+                </div>
               </li>
             ))}
           </ul>
