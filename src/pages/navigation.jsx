6a27a998171e20f0e98761f0e79186802b493694
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-01-10 16:53:11 -0300

    minor style changes

--- src/pages/navigation.jsx
+++ src/pages/navigation.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 const Navigation = () => (
   <>
-    <div className="fixed top-0 left-0 right-0 h-20 border-b-2 bg-slate-100">
+    <div className="fixed top-0 left-0 right-0 z-50 h-20 border-b-2 bg-slate-100">
       <nav>
         <ul>
           <li><NavLink to="/">Home</NavLink></li>
