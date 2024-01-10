d4d8c056beda4a191edd98da964c7cd63ae3cc11
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-01-10 18:19:12 -0300

    add basic styles to navbar

--- src/pages/navigation.jsx
+++ src/pages/navigation.jsx
@@ -1,13 +1,17 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import { NavLink } from 'react-router-dom';
+import appLogo from '../images/app_logo.png';
+import profile from '../images/profile.png';
 
 const Navigation = () => (
   <>
     <div className="fixed top-0 left-0 right-0 z-50 h-20 border-b-2 bg-slate-100">
-      <nav>
-        <ul>
-          <li><NavLink to="/">Home</NavLink></li>
-          <li><NavLink to="/contact">Contact</NavLink></li>
+      <nav className="h-full p-2">
+        <ul className="flex flex-row justify-between h-full pr-4">
+          <li className=""><NavLink to="/"><img src={appLogo} alt="logo" className="h-full" /></NavLink></li>
+          <li className="flex flex-col justify-center"><NavLink to="/">Products</NavLink></li>
+          <li className="flex flex-col justify-center"><NavLink to="/contact">Contact</NavLink></li>
+          <li className="flex flex-col justify-center"><NavLink to="/contact">Profile</NavLink></li>
+          <li className=""><NavLink to="/contact"><img src={profile} alt="logo" className="mt-4 h-1/2" /></NavLink></li>
         </ul>
       </nav>
     </div>
