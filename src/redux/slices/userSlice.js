e6c29b624a8cbd12a890d7238fd636294f4a579a
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-01-29 23:24:15 -0300

    Add createGuestUser action to userSlice

--- src/redux/slices/userSlice.js
+++ src/redux/slices/userSlice.js
@@ -15,6 +15,15 @@ export const fetchUser = createAsyncThunk('user/show', async () => {
   }
 });
 
+export const createGuestUser = createAsyncThunk('user/create', async () => {
+  try {
+    const response = await axios.post(`${url}users/create_temporary_user`);
+    return response.data;
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
 const initialState = {
   loading: true,
   userFetched: false,
@@ -35,13 +44,23 @@ const userSlice = createSlice({
     },
   },
   extraReducers: (builder) => {
-    builder.addCase(fetchUser.fulfilled, (state, action) => {
-      state.loading = false;
-      state.user = action.payload[0];
-      state.cart = action.payload[1];
-      state.oldCartItems = action.payload[2];
-      state.userFetched = true;
-    });
+    builder
+      .addCase(fetchUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.user = action.payload[0];
+        state.cart = action.payload[1];
+        state.oldCartItems = action.payload[2];
+        state.userFetched = true;
+      })
+      .addCase(createGuestUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.user = action.payload[0];
+        state.cart = action.payload[1];
+        state.oldCartItems = action.payload[2];
+        state.userFetched = true;
+        console.log('Guest user created');
+        console.log(action.payload[2]);
+      });
   },
 });
 
