9038f5b91a49463c7268727095745c5ac3abace3
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-02-02 14:21:33 -0300

    Refactor Cart component to improve performance and readability

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -10,19 +10,25 @@ import { createGuestUser, fetchUser } from '../redux/slices/userSlice';
 const Cart = () => {
   const {
     loading,
-    user,
-    cart,
-    orderItems,
   } = useSelector((store) => store.user);
 
-  const itemsArr = orderItems;
+  const {
+    orderItems,
+    itemsCount,
+  } = useSelector((store) => store.order);
 
-  if (loading) {
+  const {
+    itemsArr,
+    itemsLoading,
+  } = useSelector((store) => store.items);
+
+  if (loading || itemsLoading) {
     return (
       <p>loading....</p>
     );
   }
-  if (itemsArr.length === 0) {
+
+  if (itemsCount === 0) {
     return (
       <section className="flex flex-col gap-8 pt-8 text-center">
         <p>Your cart Is empty</p>
@@ -37,35 +43,39 @@ const Cart = () => {
   return (
     <section className="flex flex-row justify-center">
       <ul className="flex flex-col w-10/12 gap-16 py-10">
-        {itemsArr.map((item) => (
-          <li key={item.id} className="border rounded-2xl">
-            <div className="py-2 text-center">
-              <p className="text-2xl">{item.name}</p>
-            </div>
-            <div className="flex flex-row gap-2">
-              <div className="w-1/2">
-                <img src={item.image} alt={`${item.name}`} />
-              </div>
-              <div className="flex flex-col justify-between text-center">
-                <div className="">
-                  <p>{item.description}</p>
+        {itemsArr.map((item) => {
+          if (orderItems[item.name]) {
+            return (
+              <li key={item.id} className="border rounded-2xl">
+                <div className="py-2 text-center">
+                  <p className="text-2xl">{item.name}</p>
                 </div>
-                <div className="flex justify-around">
-                  <button type="button" className="px-3 py-1 bg-red-400 rounded-full"> Delete</button>
+                <div className="flex flex-row gap-2">
+                  <div className="w-1/2">
+                    <img src={item.image} alt={`${item.name}`} />
+                  </div>
+                  <div className="flex flex-col justify-between text-center">
+                    <div className="">
+                      <p>{item.description}</p>
+                    </div>
+                    <div className="flex justify-around">
+                      <button type="button" className="px-3 py-1 bg-red-400 rounded-full"> Delete</button>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-
-            <div className="flex flex-row justify-around py-4">
-              <div className="flex flex-row justify-center">
-                <button type="button" className="text-4xl border-s border-y ">-</button>
-                <p className="">1</p>
-                <button type="button" className="">+</button>
-              </div>
-              <p>{item.price}</p>
-            </div>
-          </li>
-        ))}
+                <div className="flex flex-row justify-around py-4">
+                  <div className="flex flex-row justify-center">
+                    <button type="button" className="text-4xl border-s border-y ">-</button>
+                    <p className="">1</p>
+                    <button type="button" className="">+</button>
+                  </div>
+                  <p>{item.price}</p>
+                </div>
+              </li>
+            );
+          }
+          return null;
+        })}
       </ul>
     </section>
   );
