5a72db0d72f3dfeb513c2cd658bd9f68d63eb8d3
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-02-05 18:58:45 -0300

    add custom colors and fonts to tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,24 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'proxima-nova': ['Proxima Nova', 'Arial', 'sans-serif'],
+      },
+      colors: {
+        'oxford-blue': '#0b132b',
+        'space-cadet': '#1c2541',
+        'ym-blue': '#3a506b',
+        verdigris: '#5bc0be',
+        'fluorescent-cyan': '#6fffe9',
+        'semi-transparent-light-gray': 'rgba(211, 220, 230, 0.4)',
+        'transparent-white': 'rgba(255, 255, 255, 0.5)',
+        'extra-transparent-indigo': 'rgba(54, 5, 104, 0.9)',
+        'semi-transparent-indigo': 'rgba(54, 5, 104, 0.7)',
+        'extra-transparent-tekheltet': 'rgba(91, 42, 134, 0.4)',
+        'semi-transparent-tekheltet': 'rgba(91, 42, 134, 0.5)',
+      },
+    },
   },
   plugins: [],
 };
