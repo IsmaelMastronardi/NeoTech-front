f7e6e2054a3b8c897e5687126b073088c71bc13f
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-02-04 21:16:44 -0300

    Refactor Profile component to include loading state and render PastOrders component

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,25 @@
-const Profile = () => (
-  <p>Profile</p>
-);
+/* eslint-disable no-unused-vars */
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchPastOrders } from '../redux/slices/orderSlice';
+import PastOrders from '../components/pastOrders';
+
+const Profile = () => {
+  const dispatch = useDispatch();
+  const { pastOrdersLoading, pastOrdersArray } = useSelector((store) => store.order);
+  const { user, loading } = useSelector((store) => store.user);
+
+  if (loading) {
+    return (
+      <p>Loading...</p>
+    );
+  }
+
+  return (
+    <section className="flex flex-col items-center justify-center">
+      <PastOrders />
+    </section>
+  );
+};
 
 export default Profile;
