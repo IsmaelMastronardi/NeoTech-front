ee968c9bcff22444144e2361c12e59eaa5f3878e
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-02-05 12:33:01 -0300

    Remove unused imports and variables in Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,9 @@
 /* eslint-disable no-unused-vars */
-import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchPastOrders } from '../redux/slices/orderSlice';
 import PastOrders from '../components/pastOrders';
 
 const Profile = () => {
   const dispatch = useDispatch();
-  const { pastOrdersLoading, pastOrdersArray } = useSelector((store) => store.order);
   const { user, loading } = useSelector((store) => store.user);
 
   if (loading) {
